276cf460f853624fe6c6c3d8f05cbcc4d4da9d24
Juraj Szitas <szitas.juraj13@gmail.com>
2023-10-03 02:11:33 +0200

    Update examples

--- example.cpp
+++ example.cpp
@@ -1,19 +1,37 @@
-#include "nlsolver.h"
+// Copyright (c) 2023- Juraj Szitas
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "nlsolver.h"  // NOLINT
 
 // baseline 'tried and tested' solvers
-using nlsolver::NelderMeadSolver;
 using nlsolver::DESolver;
+using nlsolver::GradientDescent;
+using nlsolver::NelderMeadPSO;
+using nlsolver::NelderMeadSolver;
 using nlsolver::PSOSolver;
 using nlsolver::SANNSolver;
 using nlsolver::rng::xorshift;
 using nlsolver::rng::xoshiro;
-using nlsolver::NelderMeadPSO;
+using GDType = nlsolver::GradientStepType;
 
-// experimental solvers 
-using nlsolver::experimental::GradientDescent;
+// experimental solvers
 
 class Rosenbrock {
-public:
+ public:
   double operator()(std::vector<double> &x) {
     const double t1 = 1 - x[0];
     const double t2 = (x[1] - x[0] * x[0]);
@@ -22,33 +40,27 @@ public:
 };
 
 void print_vector(std::vector<double> &x) {
-  if( x.size() < 1 ) {
-    return;
+  for (auto &val : x) {
+    std::cout << val << ",";
   }
-  size_t i = 0;
-  for(; i < x.size() - 1; i++ ) {
-    std::cout << x[i] << ",";
-  }
-  i++;
-  std::cout << x[i] << std::endl;
+  std::cout << "\n";
 }
 
-// to use any C++ standard random number generator just pass in a generator 
-// functor e.g. using Mersene Twister 
+// to use any C++ standard random number generator just pass in a generator
+// functor e.g. using Mersene Twister
 #include <cstdint>
 #include <random>
 struct std_MT {
-private:
+ private:
   std::mt19937 generator;
   std::uniform_real_distribution<double> distribution;
-public:
-  std_MT() {
-    this->generator = std::mt19937();
-    this->distribution = std::uniform_real_distribution<double>(0.0,1.0);
+
+ public:
+  std_MT() {                           // NOLINT
+    this->generator = std::mt19937();  // NOLINT
+    this->distribution = std::uniform_real_distribution<double>(0.0, 1.0);
   }
-  double operator()() {
-    return this->distribution(this->generator);
-  }
+  double operator()() { return this->distribution(this->generator); }
 };
 
 int main() {
@@ -58,31 +70,31 @@ int main() {
   // initialize solver - passing the functor
   auto nm_solver = NelderMeadSolver<Rosenbrock>(prob);
   // initialize function arguments
-  std::vector<double> nm_init = {2,7};
+  std::vector<double> nm_init = {2, 7};
   auto nm_res = nm_solver.minimize(nm_init);
-  // check solver status 
+  // check solver status
   nm_res.print();
   // and estimated function parameters
   print_vector(nm_init);
-  
+
   std::cout << "Nelder-Mead using a lambda function: " << std::endl;
-  // try the same with a lambda function 
-  auto RosenbrockLambda = [](std::vector<double> &x){
+  // try the same with a lambda function
+  auto RosenbrockLambda = [](std::vector<double> &x) {
     const double t1 = 1 - x[0];
     const double t2 = (x[1] - x[0] * x[0]);
     return t1 * t1 + 100 * t2 * t2;
   };
   auto nm_solver_lambda = NelderMeadSolver(RosenbrockLambda);
   // initialize function arguments
-  nm_init = {2,7};
+  nm_init = {2, 7};
   // nm_init[0] = 2;
   // nm_init[1] = 7;
   auto nm_res_lambda = nm_solver_lambda.minimize(nm_init);
-  // check solver status 
+  // check solver status
   nm_res_lambda.print();
   // and estimated function parameters
   print_vector(nm_init);
-  
+
   // use recombination strategy
   using DEStrat = nlsolver::RecombinationStrategy;
   std::cout << "Differential evolution with xorshift: " << std::endl;
@@ -90,99 +102,120 @@ int main() {
   // include some, including a xorshift RNG, and a xoshiro RNG
   xorshift<double> gen;
   // again initialize solver, this time also with the RNG
-  auto de_solver = DESolver<Rosenbrock, xorshift<double>, double, DEStrat::best> (prob, gen);
-  
-  std::vector<double> de_init = {2,7};;
+  auto de_solver =
+      DESolver<Rosenbrock, xorshift<double>, double, DEStrat::best>(prob, gen);
+
+  std::vector<double> de_init = {2, 7};
+
   auto de_res = de_solver.minimize(de_init);
   de_res.print();
   print_vector(de_init);
-  
+
   std::cout << "Differential evolution with std::mt19937: " << std::endl;
   // using standard library random number generators
   std_MT std_gen;
   // again initialize solver, this time also with the RNG
-  // if strategy is not specifieed, defaults to random 
-  auto de_solver_MT = DESolver<Rosenbrock, std_MT, double> (prob, std_gen);
+  // if strategy is not specifieed, defaults to random
+  auto de_solver_MT = DESolver<Rosenbrock, std_MT, double>(prob, std_gen);
   // reset initial state
-  de_init[0] = 2; de_init[1] = 7;
+  de_init[0] = 2;
+  de_init[1] = 7;
   de_res = de_solver_MT.minimize(de_init);
   de_res.print();
   print_vector(de_init);
-  
+
   std::cout << "Particle Swarm Optimization with xoshiro: " << std::endl;
   // we also have a xoshiro generator
   xoshiro<double> xos_gen;
-  
-  // add PSO Solver Type - defaults to random 
+
+  // add PSO Solver Type - defaults to random
   using nlsolver::PSOType;
   // again initialize solver, this time also with the RNG
-  auto pso_solver = PSOSolver<Rosenbrock,
-                              xoshiro<double>,
-                              double> (prob, xos_gen);
-  // set initial state - if no bounds are given, default initial parameters are 
+  auto pso_solver =
+      PSOSolver<Rosenbrock, xoshiro<double>, double>(prob, xos_gen);
+  // set initial state - if no bounds are given, default initial parameters are
   // taken roughly as the scale of the parameter space
-  std::vector<double> pso_init = {3,3};
+  std::vector<double> pso_init = {3, 3};
   auto pso_res = pso_solver.minimize(pso_init);
   pso_res.print();
   print_vector(pso_init);
-  std::cout << "Particle Swarm Optimization with xoshiro (and bounds): " << std::endl;
-  // this tends to be much worse than not specifying bounds for PSO - so 
-  // we heavily recommend those: 
+  std::cout << "Particle Swarm Optimization with xoshiro (and bounds): "
+            << std::endl;
+  // this tends to be much worse than not specifying bounds for PSO - so
+  // we heavily recommend those:
   pso_init[0] = 0;
   pso_init[1] = 0;
-  std::vector<double> pso_lower = {-1,-1};
-  std::vector<double> pso_upper = {1,1};
-  
+  std::vector<double> pso_lower = {-1, -1};
+  std::vector<double> pso_upper = {1, 1};
+
   pso_res = pso_solver.minimize(pso_init, pso_lower, pso_upper);
   pso_res.print();
   print_vector(pso_init);
-  std::cout << "Accelerated Particle Swarm Optimization with xoshiro: " << std::endl;
-  // we also have an accelerated version - we reset the RNG as well. 
+  std::cout << "Accelerated Particle Swarm Optimization with xoshiro: "
+            << std::endl;
+  // we also have an accelerated version - we reset the RNG as well.
   xos_gen.reset();
-  auto apso_solver = PSOSolver<Rosenbrock,
-                               xoshiro<double>,
-                               double,
-                               PSOType::Accelerated> (prob, xos_gen);
-  // set initial state - if no bounds are given, default initial parameters are 
+  auto apso_solver =
+      PSOSolver<Rosenbrock, xoshiro<double>, double, PSOType::Accelerated>(
+          prob, xos_gen);
+  // set initial state - if no bounds are given, default initial parameters are
   // taken roughly as the scale of the parameter space
-  std::vector<double> apso_init = {3,3};
+  std::vector<double> apso_init = {3, 3};
   auto apso_res = apso_solver.minimize(apso_init);
   apso_res.print();
   print_vector(apso_init);
-  
+
   std::cout << "Simulated Annealing with xoshiro: " << std::endl;
-  // we also have an accelerated version - we reset the RNG as well. 
+  // we also have an accelerated version - we reset the RNG as well.
   xos_gen.reset();
-  auto sann_solver = SANNSolver<Rosenbrock,
-                                xoshiro<double>,
-                                double> (prob, xos_gen);
-  // set initial state - if no bounds are given, default initial parameters are 
+  auto sann_solver =
+      SANNSolver<Rosenbrock, xoshiro<double>, double>(prob, xos_gen);
+  // set initial state - if no bounds are given, default initial parameters are
   // taken roughly as the scale of the parameter space
-  std::vector<double> sann_init = {3,3};
+  std::vector<double> sann_init = {3, 3};
   auto sann_res = sann_solver.minimize(sann_init);
   sann_res.print();
   print_vector(sann_init);
-  
+
   std::cout << "NelderMead-PSO hybrid with xoshiro: " << std::endl;
-  // we also have an accelerated version - we reset the RNG as well. 
+  // we also have an accelerated version - we reset the RNG as well.
   xos_gen.reset();
-  auto nm_pso_solver = NelderMeadPSO<Rosenbrock,
-                                     xoshiro<double>,
-                                     double> (prob, xos_gen);
-  // set initial state - if no bounds are given, default initial parameters are 
+  auto nm_pso_solver =
+      NelderMeadPSO<Rosenbrock, xoshiro<double>, double>(prob, xos_gen);
+  // set initial state - if no bounds are given, default initial parameters are
   // taken roughly as the scale of the parameter space
-  std::vector<double> nm_pso_init = {3,3};
+  std::vector<double> nm_pso_init = {3, 3};
   auto nm_pso_res = nm_pso_solver.minimize(nm_pso_init);
   nm_pso_res.print();
   print_vector(nm_pso_init);
-  
-  std::cout << "Gradient Descent without linesearch: " << std::endl;
-  auto gd_solver = GradientDescent<Rosenbrock, double, false> (prob, 0.0001, 100);
-  std::vector<double> gd_init = {2,2};
-  auto gd_res = gd_solver.minimize(gd_init);
-  gd_res.print();
-  print_vector(gd_init);
-  
+
+  std::cout << "Gradient Descent without line-search using fixed steps: "
+            << std::endl;
+  auto gd_solver_fixed =
+      GradientDescent<Rosenbrock, double, GDType::Bigstep>(prob);
+  std::vector<double> gd_init_fixed = {2, 2};
+  auto gd_res_fixed = gd_solver_fixed.minimize(gd_init_fixed);
+  gd_res_fixed.print();
+  print_vector(gd_init_fixed);
+
+  std::cout << "Gradient Descent with line-search: " << std::endl;
+  auto gd_solver_linesearch =
+      GradientDescent<Rosenbrock, double, GDType::Linesearch>(prob);
+  std::vector<double> gd_init_linesearch = {2, 2};
+  auto gd_res_linesearch = gd_solver_linesearch.minimize(gd_init_linesearch);
+  gd_res_linesearch.print();
+  print_vector(gd_init_linesearch);
+
+  std::cout
+      << "Gradient Descent without line-search using big steps,"
+      << " cycling through step-sizes (and lipschitz constant eyeballing): "
+      << std::endl;
+  auto gd_solver_bigstep =
+      GradientDescent<Rosenbrock, double, GDType::Bigstep, 5, true>(prob);
+  std::vector<double> gd_init_bigstep = {2, 2};
+  auto gd_res_bigstep = gd_solver_bigstep.minimize(gd_init_bigstep);
+  gd_res_bigstep.print();
+  print_vector(gd_init_bigstep);
+
   return 0;
 }
-
